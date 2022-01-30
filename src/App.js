6a8cc349467922332d72f4dfe7af61fedaff000f
Muhammad Talha <mtalha4600@gmail.com>
2022-01-30 16:28:28 +0500

    card section 2 is complete

--- src/App.js
+++ src/App.js
@@ -16,7 +16,11 @@ import Card_1 from './componenets/Card1/Card_1';
 import Card_2 from './componenets/Card2/Card_2';
 import Card_3 from './componenets/Card3/Card_3';
 import Udcard from './componenets/undercards/Udcard';
-
+import Card4 from './componenets/Card4/Card4'
+import Card5 from './componenets/Card5/Card5';
+import Card6 from './componenets/Card6/Card6';
+import Card7 from './componenets/Card7/Card7';
+import arrow from './assets/images/arrow.png'
 
 
 function App() {
@@ -86,11 +90,36 @@ function App() {
 
 
     <div className='container under_cardx'>
+
       <div className='row'>
         <div className='col-5'> <Udcard/> </div>
         <div className='col-3'></div>
         <div className='col-2'> <Buttonblue width="180px" height="40px" background="white" color="black" title="Property Type"/> </div>
         <div className='col-2'> <Buttonblue width="164px" height="40px"  title="See All Property"/> </div>
+      </div>
+    </div>
+
+    {/* width= "84px"height="30px" */}
+
+
+    <div className='container card_part2'>
+
+      <div className='row card_4_5'>
+        <div className='col-6 inner_set1'> <Card4/> </div>
+        <div className='col-6 inner_set2'>  <Card5/> </div>
+      </div>
+
+      <div className='row card_6_7'>
+        <div className='col-6 inner_set3 '> <Card6/> </div>
+        <div className='col-6 inner_set4'>  <Card7/> </div>
+      </div>
+
+      <div className='container arow_containr'> 
+        <div className='row'>
+          <div className='col-6'></div>
+          <div className='col-1 arroww'> <img src={arrow}  /> </div>
+          <div className='col-5'></div>
+        </div>
       </div>
     </div>
    
--- src/componenets/Buttonblue/Buttonblue.js
+++ src/componenets/Buttonblue/Buttonblue.js
@@ -5,7 +5,7 @@ const Buttonblue = (props) => {
     return (
         <div className='btnblue'>
 
-            <button type="button" class="btn btn-primary" style={{color:props.color,width:props.width ,background:props.background} }>{props.title}</button>
+            <button type="button" class="btn btn-primary" style={{color:props.color,width:props.width ,background:props.background , font:props.font} }>{props.title}</button>
         
         </div>
     );
